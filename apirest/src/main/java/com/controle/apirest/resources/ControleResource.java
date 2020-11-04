7f44a1c7fc4ec859cce6806a70efacede25e02f7
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2020-11-04 01:11:07 -0400

    deploy no heroko

--- apirest/src/main/java/com/controle/apirest/resources/ControleResource.java
+++ apirest/src/main/java/com/controle/apirest/resources/ControleResource.java
@@ -3,6 +3,7 @@ package com.controle.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.controle.apirest.models.Controle;
 import com.controle.apirest.repository.ControleRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 //requisicoes do http
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Controle")
+@CrossOrigin(origins="*")         //qualquer dominio pode acessar essa api
 public class ControleResource {
 	
 	//criar ponto de injecao para utilizar o metodo e conectar com o bd
@@ -26,29 +32,33 @@ public class ControleResource {
 	
 	//listar os estagiarios
 	@GetMapping("/estagiarios")
+	@ApiOperation(value="Retorna uma lista de estagiarios")
 	public List<Controle> listaEstagiarios(){
 		return controleRepository.findAll();
 	}
 	//listar um estagiario especifico
 	@GetMapping("/estagiarios/{id}")
+	@ApiOperation(value="Retorna um estagiario especifico")
 	public Controle listaEstagiariosUnico(@PathVariable(value="id") long id){
 		return controleRepository.findById(id);
 	}
 	
 	//criar metodo post para salvar um determinado produto
 	@PostMapping("/estagiario")
+	@ApiOperation(value="Salva um estagiario")
 	public Controle salvarEstagiario(@RequestBody Controle controle) {
 		return controleRepository.save(controle);
 	}
 	
-	
 	//criar metodo delete para deletar um determinado produto
 	@DeleteMapping("/estagiario")
+	@ApiOperation(value="Deleta um lista de estagiario")
 	public void atualizaEstagiario(@RequestBody Controle controle) {
 		controleRepository.delete(controle);
 	}
 	//criar metodo put para atualizar um determinado produto
 		@PutMapping("/estagiario")
+		@ApiOperation(value="Atualiza um estagiario")
 		public Controle deletaEstagiario(@RequestBody Controle controle) {
 			return controleRepository.save(controle);
 		}
